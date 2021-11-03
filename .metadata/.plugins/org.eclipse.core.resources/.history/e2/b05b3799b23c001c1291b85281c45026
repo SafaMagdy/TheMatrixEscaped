bd931a1a6ac46ab92d9f4bc3155e4372835b7f05
SafaMagdy <safamagdy1999@gmail.com>
2021-11-04 01:02:42 +0200

    7assa b inefficiency

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e2/b05b3799b23c001c1291b85281c45026
@@ -0,0 +1,6 @@
+package code;
+import code.Location;
+
+public class Matrix {
+
+}
--- AI/src/code/Matrix.java
+++ AI/src/code/Matrix.java
@@ -2,5 +2,22 @@ package code;
 import code.Location;
 
 public class Matrix {
+	
+	
+	//two data structures one queue to store the unexpanded semi-node
+	//the other structure is to store the already expanded
+	//create a class for action methods?
+	
+	//check current cell components (take a pill)
+	
+	//use array list instead of queue
+	
+	
+	//gen grid
+	//solve
+	//method to detect possible actions and insert in queue
+	//update
+	//method to check goal test
+	
 
 }
